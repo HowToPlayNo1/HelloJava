1148f185d520211bcd1d05dbdf383c610f811f3d
HowToPlayNo1 <1014634026@qq.com>
2016-09-29 00:10:08 +0800

    regison test

--- src/main/java/SyntaxStudy.java
+++ src/main/java/SyntaxStudy.java
@@ -11,64 +11,67 @@ import static org.junit.Assert.*;
  */
 public class SyntaxStudy {
 
-    public String getString(){
+    public String getString() {
         return "Hello World!";
     }
-    public char getChar(){
+
+    public char getChar() {
         return 'A';
     }
 
-    public void arrayTest(){
+    public void arrayTest() {
 
     }
 
     @Test
-    public void primitiveTypeTest(){
+    public void primitiveTypeTest() {
 
+        //region java string
         System.out.println("java 字符串:");
-        System.out.printf("this is String Type:%s,getClass:%s \r\n",getString(),getString().getClass());
+        System.out.printf("this is String Type:%s,getClass:%s \r\n", getString(), getString().getClass());
+        //endregion
 
         System.out.println("java char :");
-        System.out.printf("this is char Type:%c \r\n",getChar());
+        System.out.printf("this is char Type:%c \r\n", getChar());
 
         System.out.println("java 4 种整型:");
-        System.out.printf("this is int Type:%d,4字节 \r\n",4);
-        System.out.printf("this is short Type:%d,2字节 \r\n",2);
-        System.out.printf("this is long Type:%d,8字节 \r\n",8);
-        System.out.printf("this is byte Type:%d,1字节 \r\n",1);
+        System.out.printf("this is int Type:%d,4字节 \r\n", 4);
+        System.out.printf("this is short Type:%d,2字节 \r\n", 2);
+        System.out.printf("this is long Type:%d,8字节 \r\n", 8);
+        System.out.printf("this is byte Type:%d,1字节 \r\n", 1);
 
         System.out.println("java boolean:");
         boolean b = true;
-        System.out.printf("this is boolean Type:%b, \r\n",b, b);
+        System.out.printf("this is boolean Type:%b, \r\n", b, b);
 
     }
 
     @Test
-    public void stringTest(){
-        String str=null;
+    public void stringTest() {
+        String str = null;
 
-        str=String.format("Hi,%s", "王力");
+        str = String.format("Hi,%s", "王力");
         System.out.println(str);
-        str=String.format("Hi,%s:%s.%s", "王南","王力","王张");
+        str = String.format("Hi,%s:%s.%s", "王南", "王力", "王张");
         System.out.println(str);
 
         System.out.printf("字母a的大写是：%c %n", 'A');
-        System.out.printf("3>7的结果是：%b %n", 3>7);
-        System.out.printf("100的一半是：%d %n", 100/2);
+        System.out.printf("3>7的结果是：%b %n", 3 > 7);
+        System.out.printf("100的一半是：%d %n", 100 / 2);
         System.out.printf("100的16进制数是：%x %n", 100);
         System.out.printf("100的8进制数是：%o %n", 100);
-        System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
-        System.out.printf("上面价格的16进制数是：%a %n", 50*0.85);
-        System.out.printf("上面价格的指数表示：%e %n", 50*0.85);
-        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
+        System.out.printf("50元的书打8.5折扣是：%f 元%n", 50 * 0.85);
+        System.out.printf("上面价格的16进制数是：%a %n", 50 * 0.85);
+        System.out.printf("上面价格的指数表示：%e %n", 50 * 0.85);
+        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50 * 0.85);
         System.out.printf("上面的折扣是%d%% %n", 85);
         System.out.printf("字母A的散列码是：%h %n %n", 'A');
 
         //$使用
-        str=String.format("格式参数$的使用：%1$d,%2$s", 99,"abc");
+        str = String.format("格式参数$的使用：%1$d,%2$s", 99, "abc");
         System.out.println(str);
         //+使用
-        System.out.printf("显示正负数的符号：%+d与%d%n", 99,-99);
+        System.out.printf("显示正负数的符号：%+d与%d%n", 99, -99);
         //补O使用
         System.out.printf("最牛的编号是：%03d%n", 7);
         //空格使用
@@ -80,46 +83,46 @@ public class SyntaxStudy {
     }
 
     @Test
-    public void dateStringFormatTest(){
-        Date date=new Date();
+    public void dateStringFormatTest() {
+        Date date = new Date();
         //c的使用
-        System.out.printf("全部日期和时间信息：%tc%n",date);
+        System.out.printf("全部日期和时间信息：%tc%n", date);
         //f的使用
-        System.out.printf("年-月-日格式：%tF%n",date);
+        System.out.printf("年-月-日格式：%tF%n", date);
         //d的使用
-        System.out.printf("月/日/年格式：%tD%n",date);
+        System.out.printf("月/日/年格式：%tD%n", date);
         //r的使用
-        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n",date);
+        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n", date);
         //t的使用
-        System.out.printf("HH:MM:SS格式（24时制）：%tT%n",date);
+        System.out.printf("HH:MM:SS格式（24时制）：%tT%n", date);
         //R的使用
-        System.out.printf("HH:MM格式（24时制）：%tR %n%n",date);
+        System.out.printf("HH:MM格式（24时制）：%tR %n%n", date);
 
         //b的使用，月份简称
-        String str=String.format(Locale.US,"英文月份简称：%tb",date);
+        String str = String.format(Locale.US, "英文月份简称：%tb", date);
         System.out.println(str);
-        System.out.printf("本地月份简称：%tb%n",date);
+        System.out.printf("本地月份简称：%tb%n", date);
         //B的使用，月份全称
-        str=String.format(Locale.US,"英文月份全称：%tB",date);
+        str = String.format(Locale.US, "英文月份全称：%tB", date);
         System.out.println(str);
-        System.out.printf("本地月份全称：%tB%n",date);
+        System.out.printf("本地月份全称：%tB%n", date);
         //a的使用，星期简称
-        str=String.format(Locale.US,"英文星期的简称：%ta",date);
+        str = String.format(Locale.US, "英文星期的简称：%ta", date);
         System.out.println(str);
         //A的使用，星期全称
-        System.out.printf("本地星期的简称：%tA%n",date);
+        System.out.printf("本地星期的简称：%tA%n", date);
         //C的使用，年前两位
-        System.out.printf("年的前两位数字（不足两位前面补0）：%tC%n",date);
+        System.out.printf("年的前两位数字（不足两位前面补0）：%tC%n", date);
         //y的使用，年后两位
-        System.out.printf("年的后两位数字（不足两位前面补0）：%ty%n",date);
+        System.out.printf("年的后两位数字（不足两位前面补0）：%ty%n", date);
         //j的使用，一年的天数
-        System.out.printf("一年中的天数（即年的第几天）：%tj%n",date);
+        System.out.printf("一年中的天数（即年的第几天）：%tj%n", date);
         //m的使用，月份
-        System.out.printf("两位数字的月份（不足两位前面补0）：%tm%n",date);
+        System.out.printf("两位数字的月份（不足两位前面补0）：%tm%n", date);
         //d的使用，日（二位，不够补零）
-        System.out.printf("两位数字的日（不足两位前面补0）：%td%n",date);
+        System.out.printf("两位数字的日（不足两位前面补0）：%td%n", date);
         //e的使用，日（一位不补零）
-        System.out.printf("月份的日（前面不补0）：%te %n%n",date);
+        System.out.printf("月份的日（前面不补0）：%te %n%n", date);
 
 
         //H的使用
